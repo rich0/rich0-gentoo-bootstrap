7bbb85a9a9d3eec18299cebbdc039fa29b9f2252
Eric Dowd <edowd@dowdandassociates.com>
2012-05-03 03:44:53 +0000

    Importing build scripts

---
+++ build_gentoo_64.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+
+# The region to install into
+region="us-east-1"
+
+# The security group to use. 22/tcp needs to be open
+group="default"
+
+# The ec2 key pair to use
+key="example"
+
+# The fully qualified path to private key of the ec2 key pair
+keyfile="$HOME/.ssh/example.pem"
+
+#-----
+
+building="Gentoo 64 EBS"
+start_time=`date +%Y-%m-%dT%H:%M:%S`
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: region = $region"
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: group = $group"
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: key = $key"
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: keyfile = $keyfile"
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: finding latest kernel-id"
+
+latest_kernel=`ec2-describe-images \
+--region $region \
+--filter image-type=kernel \
+--filter manifest-location=*pv-grub* \
+--owner amazon \
+--filter architecture=x86_64 \
+| grep -v "hd00" \
+| awk '{ print $3 "\t"  $2 }' \
+| sed "s:.*/pv-grub-hd0[^0-9]*::" \
+| sort \
+| tail -n 1 \
+| awk '{ print $2 }'`
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: kernel-id = $latest_kernel"
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: finding bootstrap image"
+
+boot_image=`ec2-describe-images \
+--region $region \
+--owner amazon \
+--filter architecture=x86_64 \
+--filter image-type=machine \
+--filter root-device-type=ebs \
+--filter virtualization-type=paravirtual \
+--filter kernel-id=$latest_kernel \
+--filter manifest-location=amazon/amzn-ami-* \
+| grep "^IMAGE" \
+| tail -n 1 \
+| awk '{ print $2 }'`
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: bootstrap image = $boot_image"
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: starting bootstrap instance"
+
+instance=`ec2-run-instances \
+--region $region \
+$boot_image \
+--group $group \
+--key $key \
+--instance-type c1.xlarge \
+--block-device-mapping "/dev/sdf=:10" \
+| grep "^INSTANCE" \
+| awk '{ print $2 }'`
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: bootstrap instance = $instance"
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: start checking if instance is running"
+running_check=0
+while [ $running_check -eq 0 ]; do
+    sleep 10
+    echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: checking if instance is running (10 second check)"
+    let running_check=`ec2-describe-instances \
+            --region $region \
+            $instance \
+            --filter instance-state-name=running \
+            | wc -c`
+done
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: instance is running"
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: getting hostname"
+
+server=`ec2-describe-instances \
+--region $region \
+$instance \
+--filter instance-state-name=running \
+| grep "^INSTANCE" \
+| awk '{ print $4 }'`
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: hostname = $server"
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: Wait 60 seconds, just in case, for server to finish coming up"
+
+sleep 60
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: copying remote_gentoo_64.sh to remote server"
+
+scp -o StrictHostKeyChecking=no -i $keyfile remote_gentoo_64.sh ec2-user@$server:/tmp/remote_gentoo_64.sh
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: setting remote_gentoo_64.sh as executable on remote server"
+
+ssh -o StrictHostKeyChecking=no -i $keyfile ec2-user@$server "chmod 755 /tmp/remote_gentoo_64.sh"
+ssh -o StrictHostKeyChecking=no -i $keyfile -t ec2-user@$server "sudo /tmp/remote_gentoo_64.sh"
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: checking if install is done"
+
+stopped_check=0
+while [ $stopped_check -eq 0 ]; do
+    sleep 60
+    echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: checking if install is done (1 minute check)"
+    let stopped_check=`ec2-describe-instances \
+            --region $region \
+            $instance \
+            --filter instance-state-name=stopped \
+            | wc -c`
+done
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: install is done"
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: get volume gentoo was installed on"
+
+volume=`ec2-describe-volumes \
+--region $region \
+--filter attachment.instance-id=$instance \
+--filter attachment.device=/dev/sdf \
+| grep "^VOLUME" \
+| awk '{ print $2 } '`
+
+name="Gentoo_64-bit-EBS-`date +%Y-%m-%d-%H-%M-%S`"
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: creating snapshot"
+
+snapshot=`ec2-create-snapshot \
+--region $region \
+$volume \
+--description $name \
+| awk '{ print $2 }'`
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: checking if snapshot is done"
+
+completed_check=0
+while [ $completed_check -eq 0 ]; do
+    sleep 60
+    echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: checking if snapshot is done (1 minute check)"
+    let completed_check=`ec2-describe-snapshots \
+        --region $region \
+        $snapshot \
+        --filter status=completed \
+        | wc -c`
+done
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: snapshot is done"
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: register image"
+
+gentoo_image=`ec2-register \
+--region $region \
+--name $name \
+--description "Gentoo 64-bit EBS" \
+--architecture x86_64 \
+--kernel $latest_kernel \
+--root-device-name /dev/sda1 \
+--block-device-mapping "/dev/sda1=$snapshot" \
+--block-device-mapping "/dev/sdb=ephemeral0" \
+--block-device-mapping "/dev/sdc=ephemeral1" \
+--block-device-mapping "/dev/sdd=ephemeral2" \
+--block-device-mapping "/dev/sde=ephemeral3" \
+| awk '{ print $2 }'`
+
+echo "$building $start_time - `date +%Y-%m-%dT%H:%M:%S`: image-id = $gentoo_image"
+
---
+++ remote_gentoo_64.sh
@@ -0,0 +1,192 @@
+
+echo "mkfs -t ext4 /dev/xvdf"
+mkfs -t ext4 /dev/xvdf
+echo "mkdir -p /mnt/gentoo"
+mkdir -p /mnt/gentoo
+
+mount /dev/xvdf /mnt/gentoo
+
+cd /tmp
+echo "Download stage3"
+curl -O http://gentoo.mirrors.pair.com/releases/amd64/autobuilds/`curl --silent http://gentoo.mirrors.pair.com/releases/amd64/autobuilds/latest-stage3-amd64.txt | grep stage3-amd64`
+echo "Download portage"
+curl -O http://gentoo.mirrors.pair.com/snapshots/portage-latest.tar.bz2
+echo "Unpack stage3"
+tar -xjpf /tmp/stage3-*.tar.bz2 -C /mnt/gentoo
+echo "Unpack portage"
+tar -xjf /tmp/portage*.tar.bz2 -C /mnt/gentoo/usr
+
+echo "Setup files"
+
+mkdir -p /mnt/gentoo/boot/grub
+echo "/boot/grub/menu.lst"
+cat <<'EOF'>/mnt/gentoo/boot/grub/menu.lst
+default 0
+timeout 3
+title EC2
+root (hd0)
+kernel /boot/bzImage root=/dev/xvda1 rootfstype=ext4
+EOF
+
+echo "/etc/fstab"
+cat <<'EOF'>/mnt/gentoo/etc/fstab
+/dev/xvda1 / ext4 defaults 1 1
+none /dev/pts devpts gid=5,mode=620 0 0
+none /dev/shm tmpfs defaults 0 0
+none /proc proc defaults 0 0
+none /sys sysfs defaults 0 0
+EOF
+
+mkdir -p /mnt/gentoo/etc/local.d
+echo "/etc/local.d/killall_nash-hotplug.start"
+cat <<'EOF'>/mnt/gentoo/etc/local.d/killall_nash-hotplug.start
+# /etc/local.d/killall_nash-hotplug.start
+
+killall nash-hotplug
+EOF
+chmod 755 /mnt/gentoo/etc/local.d/killall_nash-hotplug.start
+
+echo "/etc/local.d/public-keys.start"
+cat <<'EOF'>/mnt/gentoo/etc/local.d/public-keys.start
+# /etc/local.d/public-keys.start
+
+[ ! -e /home/ec2-user ] && cp -r /etc/skel /home/ec2-user && chown -R ec2-user /home/ec2-user && chgrp -R ec2-user /home/ec2-user
+if [ ! -d /home/ec2-user/.ssh ] ; then
+mkdir -p /home/ec2-user/.ssh
+chmod 700 /home/ec2-user/.ssh
+chown ec2-user /home/ec2-user/.ssh
+chgrp ec2-user /home/ec2-user/.ssh
+fi
+curl http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key > /tmp/my-key
+if [ $? -eq 0 ] ; then
+cat /tmp/my-key >> /home/ec2-user/.ssh/authorized_keys
+chmod 600 /home/ec2-user/.ssh/authorized_keys
+chown ec2-user /home/ec2-user/.ssh/authorized_keys
+chgrp ec2-user /home/ec2-user/.ssh/authorized_keys
+rm /tmp/my-key
+fi
+EOF
+chmod 755 /mnt/gentoo/etc/local.d/public-keys.start
+
+echo "/etc/local.d/public-keys.stop"
+cat <<'EOF'>/mnt/gentoo/etc/local.d/public-keys.stop
+# /etc/local.d/public-keys.stop
+
+rm -f /home/ec2-user/.ssh/authorized_keys
+EOF
+chmod 755 /mnt/gentoo/etc/local.d/public-keys.stop
+
+echo "/etc/make.conf"
+cat <<'EOF'>/mnt/gentoo/etc/make.conf
+# These settings were set by the catalyst build script that automatically
+# built this stage.
+# Please consult /usr/share/portage/config/make.conf.example for a more
+# detailed example.
+CFLAGS="-O2 -pipe"
+CXXFLAGS="${CFLAGS}"
+# WARNING: Changing your CHOST is not something that should be done lightly.
+# Please consult http://www.gentoo.org/doc/en/change-chost.xml before changing.
+CHOST="x86_64-pc-linux-gnu"
+# These are the USE flags that were used in addition to what is provided by the
+# profile used for building.
+USE="mmx sse sse2"
+MAKEOPTS="-j9"
+EOF
+
+mkdir -p /mnt/gentoo/etc/portage
+
+echo "/etc/resolv.conf"
+cp -L /etc/resolv.conf /mnt/gentoo/etc/resolv.conf
+
+mkdir -p /mnt/gentoo/etc/sudoers.d
+echo "/etc/sudoers.d/ec2-user"
+cat <<'EOF'>/mnt/gentoo/etc/sudoers.d/ec2-user
+ec2-user  ALL=(ALL) NOPASSWD:ALL
+EOF
+chmod 440 /mnt/gentoo/etc/sudoers.d/ec2-user
+
+echo "/etc/sudoers.d/_sudo"
+cat <<'EOF'>/mnt/gentoo/etc/sudoers.d/_sudo
+%sudo     ALL=(ALL) ALL
+EOF
+chmod 440 /mnt/gentoo/etc/sudoers.d/_sudo
+
+echo "/usr/src/linux/.config"
+if [ ! -f /tmp/.config ]; then 
+curl --silent -o /tmp/.config http://public.dowdandassociates.com/content/gentoo-cloud-bootstrapping-gentoo-images/amd64/usr/src/linux/.config;
+fi
+mkdir -p /mnt/gentoo/tmp
+cp /tmp/.config /mnt/gentoo/tmp/.config
+
+mkdir -p /mnt/gentoo/var/lib/portage
+echo "/var/lib/portage/world"
+cat <<'EOF'>/mnt/gentoo/var/lib/portage/world
+app-admin/logrotate
+app-admin/mcelog
+app-admin/sudo
+app-admin/syslog-ng
+app-arch/unzip
+app-editors/nano
+app-editors/vim
+app-misc/screen
+app-portage/gentoolkit
+net-misc/dhcpcd
+net-misc/ntp
+sys-fs/lvm2
+sys-fs/mdadm
+sys-kernel/gentoo-sources
+sys-process/vixie-cron
+EOF
+
+echo "/tmp/build.sh"
+
+cat <<'EOF'>/mnt/gentoo/tmp/build.sh
+#!/bin/bash
+
+env-update
+source /etc/profile
+
+emerge --sync
+
+cp /usr/share/zoneinfo/GMT /etc/localtime
+
+emerge --update --deep --with-bdeps=y --newuse world
+
+cd /usr/src/linux
+mv /tmp/.config ./.config
+yes "" | make oldconfig
+make -j9 && make -j9 modules_install
+cp -L arch/x86_64/boot/bzImage /boot/bzImage
+
+groupadd sudo
+useradd -r -m -s /bin/bash ec2-user
+
+ln -s /etc/init.d/net.lo /etc/init.d/net.eth0
+
+rc-update add net.eth0 default
+rc-update add sshd default
+rc-update add syslog-ng default
+rc-update add vixie-cron default
+rc-update add ntpd default
+rc-update add lvm boot
+rc-update add mdraid boot
+
+mv /etc/make.conf /etc/make.conf.bkup
+sed "s/MAKEOPTS=\"-j.*\"/MAKEOPTS=\"-j2\"/g" /etc/make.conf.bkup > /etc/make.conf
+rm /etc/make.conf.bkup
+
+EOF
+
+chmod 755 /mnt/gentoo/tmp/build.sh
+
+mount -t proc none /mnt/gentoo/proc
+mount --rbind /dev /mnt/gentoo/dev
+mount --rbind /dev/pts /mnt/gentoo/dev/pts
+
+chroot /mnt/gentoo /tmp/build.sh
+
+rm -fR /mnt/gentoo/tmp/*
+rm -fR /mnt/gentoo/var/tmp/*
+rm -fR /mnt/gentoo/usr/portage/distfiles/*
+
+shutdown -h now
